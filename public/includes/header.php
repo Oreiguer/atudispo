59c4cefed59a5232e0d2397dee24f6c3a7380ac7
Luis Higuera <axcen.crc@gmail.com>
2020-12-14 18:41:42 -0300

    sexto desplegue

--- public/includes/header.php
+++ public/includes/header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/aos.css">
-	<link href="fontawesome/css/all.css" rel="stylesheet">
+	<link href="public/fontawesome/css/all.css" rel="stylesheet">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 	<script type="text/javascript" src="js/jquery.js"></script>
