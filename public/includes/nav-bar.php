748a724da590a3e01d8f0a750ca5dc06d62dee3f
Luis Higuera <axcen.crc@gmail.com>
2020-12-14 19:01:54 -0300

    once desplegue

--- public/includes/nav-bar.php
+++ public/includes/nav-bar.php
@@ -1,14 +1,14 @@
 <!------ Nav-bar ------->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand h1 font-italic" href="index.php">Atudispo</a>
+            <a class="navbar-brand h1 font-italic" href="../index.php">Atudispo</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="../index.php">Inicio <span class="sr-only">(current)</span></a>
                     </li>
                     
                     <li class="nav-item">
